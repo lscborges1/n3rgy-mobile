fee7d2ebce09110ed2b5d1ed37591042cd76cc92
Lucas Borges <lscborges1@gmail.com>
2021-05-03 15:47:52 -0300

    Added preload to icons

--- App.tsx
+++ App.tsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
+// import { Asset } from 'expo-asset';
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
 import { AppStack } from './src/routes/AppStack.routes';
 import { AuthProvider } from './src/hooks/useAuth';
 
 export default function App(): JSX.Element {
+  const [isLoadingIcons, setIsLoadingIcons] = useState(false);
+
+  function cacheIcons() {
+    return Font.loadAsync(Ionicons.font);
+  }
+
   return (
-    <AuthProvider>
-      <StatusBar translucent backgroundColor="transparent" />
-      <AppStack />
-    </AuthProvider>
+    <>
+      {isLoadingIcons ? (
+        <AppLoading
+          startAsync={cacheIcons}
+          onFinish={() => setIsLoadingIcons(true)}
+          onError={console.warn}
+        />
+      ) : (
+        <AuthProvider>
+          <StatusBar translucent backgroundColor="transparent" />
+          <AppStack />
+        </AuthProvider>
+      )}
+    </>
   );
 }
+
+<AuthProvider>
+  <StatusBar translucent backgroundColor="transparent" />
+  <AppStack />
+</AuthProvider>;
